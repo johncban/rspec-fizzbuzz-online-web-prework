4856384e8dc311e8217abb61d0e8d7b8dd13696c
johncban <johncban@gmail.com>
2019-04-13 06:37:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'pry'
 
 def fizzbuzz(int)
-
-  binding.pry
 
   if int % 3 == 0
     puts "Fizz"
