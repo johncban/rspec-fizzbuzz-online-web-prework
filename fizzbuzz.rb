4a53ef86514676e85a4aba110beeeaea029728ec
johncban <johncban@gmail.com>
2019-04-13 07:57:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,11 @@
 def fizzbuzz(int)
 
   if int % 3 == 0
-    "Fizz"
+    puts "Fizz"
   elsif int % 5 == 0
-    "Buzz"
+    puts "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    puts "FizzBuzz"
   else
     nil
   end
