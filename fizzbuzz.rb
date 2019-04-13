da38150f437ab0dd987f64358258a4ff9be10794
johncban <johncban@gmail.com>
2019-04-13 08:00:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,13 @@
 def fizzbuzz(int)
 
   if int % 3 == 0
-    return "Fizz"
+    "Fizz"
   elsif int % 5 == 0
-    return "Buzz"
+    "Buzz"
   elsif int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
+    "FizzBuzz"
   else
     nil
   end
+
 end
