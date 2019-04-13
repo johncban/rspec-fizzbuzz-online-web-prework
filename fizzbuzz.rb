196be050bb5384fc3dd614efdc1623b174ed9eb8
johncban <johncban@gmail.com>
2019-04-13 06:03:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-class FizzBuzz
-  def fizzbuzz(number)
-    if number % 3==0 && number % 5==0
-      return "FizzBuzz"
-    elsif number % 3==0
-      puts "Fizz"
-    elsif number % 5==0
-      puts "Buzz"
-    else
-      nil
-    end
-  end
+def fizzbuzz
+
 end
